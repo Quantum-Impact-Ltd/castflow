d244779e9e3d4b772a6803395126563f496f5839
Txbish <kjohny030@gmail.com>
2026-05-14 01:00:03 +0500

    chore: update component files and configurations

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, EB_Garamond, Public_Sans } from 'next/font/google'
 import { Providers } from '@/providers'
 import './globals.css'
+import { cn } from "@/lib/utils";
+
+const publicSansHeading = Public_Sans({subsets:['latin'],variable:'--font-heading'});
+
+const ebGaramond = EB_Garamond({subsets:['latin'],variable:'--font-serif'});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-serif", ebGaramond.variable, publicSansHeading.variable)}>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
